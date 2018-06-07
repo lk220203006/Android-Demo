8cd075ba6bfd3cebacd35499347ffabce6d27d81
liukun <lk220203006@126.com>
2018-06-07 23:40:49 +0800

    菜单

--- 07/app/src/main/java/com/example/digitalforest/a07/MainActivity.java
+++ 07/app/src/main/java/com/example/digitalforest/a07/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.digitalforest.a07;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuBuilder;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflate = new MenuInflater(this);
+        inflate.inflate(R.menu.menu,menu);
+
+//        menu.add(Menu.NONE,0x001,1,"AAA");
+//        menu.add(Menu.NONE,0x002,3,"BBB");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case 0x001:
+                Log.i("TEST","AAA");
+                break;
+            case 0x002:
+                Log.i("TEST","BBB");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        Log.i("TEST","onPrepareOptionsMenu");
+        if (menu != null) {
+            if (menu.getClass() == MenuBuilder.class) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+                }
+            }
+        }
+        return super.onPrepareOptionsMenu(menu);
     }
 }
